53e010c483532ba13b9ddadc9b821f273e13ca32
selia1078 <871353109@qq.com>
2022-03-06 17:24:31 +0800

    update vue.config.js

--- vue-actions-deploy/vue.config.js
+++ vue-actions-deploy/vue.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-    outputDir: 'dist',
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/vue-actions-deploy/'
-      : '/'
-  }
-  
+  outputDir: 'dist',
+  publicPath: '/'
+}
